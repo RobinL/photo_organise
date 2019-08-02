f47a4bfcbbbcbfd76c1be14feac8e08aab93a643
Robin Linacre <robinlinacre@hotmail.com>
2019-08-02 14:28:09 +0100

    Update script.py

--- script.py
+++ script.py
@@ -1,7 +1,14 @@
+
+# coding: utf-8
+
+# In[8]:
+
 import pyexiv2
+from dateutil import parser
+import pymediainfo
 
 
-# In[5]:
+# In[9]:
 
 import re
 def get_folder(metadata):
@@ -28,11 +35,21 @@ def get_folder(metadata):
     return final_string
 
 
-# In[6]:
+# In[10]:
+
+def get_folder_mp4(media_info):
+    try:
+        return parser.parse(media_info.to_data()['tracks'][0]['encoded_date'].replace("UTC ", "")).strftime('%Y-%m') + " Videos"
+    except:
+        return 'Date unknown'
+        
+
+
+# In[12]:
 
 import os
 import shutil
-base = r"F:\Digital photos new"
+base = r"C:\Users\Robin\Pictures"
 new = r"F:\Digital photos new 2"
 
 
@@ -46,17 +63,21 @@ for root, subdirs, files in os.walk(base):
     files = sorted(files, key=lambda x: ".jpg" not in x.lower())
   
     for f in files:
+ 
         counter +=1
         if counter % 200 == 0:
             print counter
         try:
             if "thumbs.db" in f.lower() or "thumbnail" in f.lower():
                 continue
+            
+            if ".mp4" not in f.lower():
+                continue
 
             depth = root.count(os.sep) - base.count(os.sep)
 
             this_file = os.path.join(root,f)
-            if ".jpg" in f.lower():
+            if ".jpg" in f.lower() or ".cr2" in f.lower():
                 metadata = pyexiv2.ImageMetadata(this_file)
                 metadata.read()
                 new_folder = get_folder(metadata)
@@ -65,13 +86,32 @@ for root, subdirs, files in os.walk(base):
                     os.makedirs(os.path.join(new,new_folder))
                 except:
                     pass
+                
+            if ".mp4" in f.lower():
+                media_info = pymediainfo.MediaInfo.parse(this_file)
+                new_folder = get_folder_mp4(media_info)
+                try:
+                    os.makedirs(os.path.join(new,new_folder))
+                except:
+                    pass
+                print(f)
+                print(new_folder)
+                print("---")
+                
+                
 
             if "good" in this_file:
                 f = f.replace("good","")
                 f = "00good" + f
                 f = f.replace(r" .", ".")
+                
+                
             if not os.path.exists(os.path.join(new,new_folder,f)):
                 shutil.copy(this_file, os.path.join(new,new_folder,f))
+
         except Exception, err:
             print "{}".format(this_file)
             print(traceback.format_exc())
+            
+        
+
